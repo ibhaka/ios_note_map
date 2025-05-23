a1d347eda62539159afb013bce9d063cae7bce56
İbrahim Halil  KALKAN <91983685+ibhaka@users.noreply.github.com>
2025-05-24 02:52:58 +0300

    created

---
+++ note-map/note-map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  note-map
+//
+//  Created by İbrahim Halil Kalkan on 24.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ note-map/note-mapTests/note_mapTests.swift
@@ -0,0 +1,17 @@
+//
+//  note_mapTests.swift
+//  note-mapTests
+//
+//  Created by İbrahim Halil Kalkan on 24.05.2025.
+//
+
+import Testing
+@testable import note_map
+
+struct note_mapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
